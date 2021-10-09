7f8a9e937ebd107b727b8b22fce3d68ff22709c7
汪恳 <1174843091@qq.com>
2021-10-09 19:21:32 +0800

    添加异常处理

--- ProcurementUndeliveredReminderTool/01.UserInterfaceLayer/MainModule.vb
+++ ProcurementUndeliveredReminderTool/01.UserInterfaceLayer/MainModule.vb
@@ -41,92 +41,98 @@ Module MainModule
             End If
             AppSettingHelper.Instance.LastSendDate = Now.Date
 
-            AppSettingHelper.Instance.Logger.Info("自动发送通知")
-            Console.WriteLine($"{Now:G}> 自动发送通知")
+            Try
 
-            Console.WriteLine($"{Now:G}> 清理临时文件")
-            AppSettingHelper.Instance.ClearTempFiles()
+                AppSettingHelper.Instance.Logger.Info("自动发送通知")
+                Console.WriteLine($"{Now:G}> 自动发送通知")
 
-            Console.WriteLine($"{Now:G}> 搜索表单数据")
-            Dim tmpDictionary = RemoteDatabaseHelper.GetDocumentItems()
+                Console.WriteLine($"{Now:G}> 清理临时文件")
+                AppSettingHelper.Instance.ClearTempFiles()
 
-            Console.WriteLine($"{Now:G}> 获取钉钉AccessToken")
-            DingTalkHelper.GetAccessToken()
+                Console.WriteLine($"{Now:G}> 搜索表单数据")
+                Dim tmpDictionary = RemoteDatabaseHelper.GetDocumentItems()
+
+                Console.WriteLine($"{Now:G}> 获取钉钉AccessToken")
+                DingTalkHelper.GetAccessToken()
 
 #Region "判断是否有无对应的钉钉账号的ERP用户"
-            If Not tmpDictionary.All(Function(s1)
-                                         Return AppSettingHelper.Instance.DingTalkUserJobNumberItems.ContainsKey(s1.Key)
-                                     End Function) Then
+                If Not tmpDictionary.All(Function(s1)
+                                             Return AppSettingHelper.Instance.DingTalkUserJobNumberItems.ContainsKey(s1.Key)
+                                         End Function) Then
 
 #Region "获取钉钉部门信息"
-                Console.WriteLine($"{Now:G}> 获取钉钉部门信息")
-                AppSettingHelper.Instance.DingTalkDepartmentIDItems.Clear()
+                    Console.WriteLine($"{Now:G}> 获取钉钉部门信息")
+                    AppSettingHelper.Instance.DingTalkDepartmentIDItems.Clear()
 
-                DingTalkHelper.GetDepartmentIDItems(1)
+                    DingTalkHelper.GetDepartmentIDItems(1)
 #End Region
 
 #Region "获取钉钉员工信息"
-                Console.WriteLine($"{Now:G}> 获取钉钉员工信息")
-                AppSettingHelper.Instance.DingTalkUserJobNumberItems.Clear()
+                    Console.WriteLine($"{Now:G}> 获取钉钉员工信息")
+                    AppSettingHelper.Instance.DingTalkUserJobNumberItems.Clear()
 
-                For Each item In AppSettingHelper.Instance.DingTalkDepartmentIDItems
-                    DingTalkHelper.GetUserItems(item)
-                Next
+                    For Each item In AppSettingHelper.Instance.DingTalkDepartmentIDItems
+                        DingTalkHelper.GetUserItems(item)
+                    Next
 #End Region
 
-            End If
+                End If
 #End Region
 
-            ' 无对应的钉钉账号的ERP用户
-            Dim NotHaveJobNumberUserItems As New Dictionary(Of String, String)
+                ' 无对应的钉钉账号的ERP用户
+                Dim NotHaveJobNumberUserItems As New Dictionary(Of String, String)
 
-            ' 发送表单数据
-            For Each item In tmpDictionary
+                ' 发送表单数据
+                For Each item In tmpDictionary
 
-                ' 钉钉限制发送频率 1500/min
-                Threading.Thread.Sleep(100)
+                    ' 钉钉限制发送频率 1500/min
+                    Threading.Thread.Sleep(100)
 
-                Console.WriteLine($"{Now:G}> 发送表单数据 {item.Key}")
+                    Console.WriteLine($"{Now:G}> 发送表单数据 {item.Key}")
 
-                ' 判断是否有对应的钉钉账号
-                If Not AppSettingHelper.Instance.DingTalkUserJobNumberItems.ContainsKey(item.Key) Then
+                    ' 判断是否有对应的钉钉账号
+                    If Not AppSettingHelper.Instance.DingTalkUserJobNumberItems.ContainsKey(item.Key) Then
 
-                    If Not NotHaveJobNumberUserItems.ContainsKey(item.Key) Then
-                        NotHaveJobNumberUserItems.Add(item.Key, item.Value.First.CGRY)
+                        If Not NotHaveJobNumberUserItems.ContainsKey(item.Key) Then
+                            NotHaveJobNumberUserItems.Add(item.Key, item.Value.First.CGRY)
 
+                        End If
+
+                        Continue For
                     End If
 
-                    Continue For
-                End If
+                    ' 生成随机文件名
+                    Dim outputFilePath = Path.Combine(AppSettingHelper.Instance.TempDirectoryPath, $"{item.Key}负责的采购物料-{Wangk.Hash.IDHelper.NewID}.xlsx")
 
-                ' 生成随机文件名
-                Dim outputFilePath = Path.Combine(AppSettingHelper.Instance.TempDirectoryPath, $"{item.Key}负责的采购物料-{Wangk.Hash.IDHelper.NewID}.xlsx")
+                    ' 生成数据文件
+                    XlsxHelper.SaveResultList(outputFilePath, item.Value)
 
-                ' 生成数据文件
-                XlsxHelper.SaveResultList(outputFilePath, item.Value)
+                    ' 上传文件到钉钉
+                    Dim tmpMediaID = DingTalkHelper.MediaUpload(outputFilePath)
 
-                ' 上传文件到钉钉
-                Dim tmpMediaID = DingTalkHelper.MediaUpload(outputFilePath)
+                    ' 发送工作通知
+                    DingTalkHelper.SendWorkMessage(AppSettingHelper.Instance.DingTalkUserJobNumberItems(item.Key), tmpMediaID)
 
-                ' 发送工作通知
-                DingTalkHelper.SendWorkMessage(AppSettingHelper.Instance.DingTalkUserJobNumberItems(item.Key), tmpMediaID)
+                    AppSettingHelper.Instance.Logger.Info($"发送通知消息至 {item.Value.First.CGRY}")
 
-                AppSettingHelper.Instance.Logger.Info($"发送通知消息至 {item.Value.First.CGRY}")
+                Next
 
-            Next
+                ' 通知管理员更新账号信息
+                If NotHaveJobNumberUserItems.Count > 0 Then
 
-            ' 通知管理员更新账号信息
-            If NotHaveJobNumberUserItems.Count > 0 Then
-
-                Dim tempAdminMessage = $"无对应的钉钉账号的ERP用户  
+                    Dim tempAdminMessage = $"无对应的钉钉账号的ERP用户  
 {String.Join(vbCrLf, From item In NotHaveJobNumberUserItems
                      Select $"> {item.Value}  ")}"
 
-                DingTalkHelper.SendMessageToAdmin(tempAdminMessage)
+                    DingTalkHelper.SendMessageToAdmin(tempAdminMessage)
 
-            End If
+                End If
 
-            Console.WriteLine($"{Now:G}> 处理完成")
+                Console.WriteLine($"{Now:G}> 处理完成")
+
+            Catch ex As Exception
+                Console.WriteLine($"{Now:G}> 处理异常 {ex}")
+            End Try
 
             'Exit Do
         Loop
